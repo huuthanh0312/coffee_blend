acef811d5a54de7b50c60626f2bf19a93226c93e
huuthanh0312 <huuthanhnguyen0312@gmail.com>
2024-03-13 10:13:28 +0700

    login and register commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,82 @@
             </div>
         </div>
     </div>
+</div> -->
+
+<section class="home-slider owl-carousel">
+
+<div class="slider-item" style="background-image: url({{ asset('assets/images/bg_1.jpg')}});" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+  <div class="container">
+    <div class="row slider-text justify-content-center align-items-center">
+
+      <div class="col-md-7 col-sm-12 text-center ftco-animate">
+          <h1 class="mb-3 mt-5 bread">Login</h1>
+          <p class="breadcrumbs"><span class="mr-2"><a href="{{ url('/') }}">Home</a></span> <span>Login</span></p>
+      </div>
+
+    </div>
+  </div>
 </div>
+</section>
+
+<section class="ftco-section">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 ftco-animate">
+        <form method="POST" action="{{ route('login') }}" class="billing-form ftco-bg-dark p-3 p-md-5">
+            @csrf
+          <h3 class="mb-4 billing-heading">Login</h3>
+            <div class="row align-items-end">
+                <div class="col-md-12">
+              <div class="form-group">
+              <label for="email" class="col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+            </div>
+           
+            <div class="col-md-12">
+              <div class="form-group">
+                <label for="password" class="col-form-label text-md-end">{{ __('Password') }}</label>
+
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+
+          </div>
+          <div class="row align-items-start">
+              <div class="form-group mt-4 col-6">
+                      <div class="radio">
+                          <button class="btn btn-primary py-3 px-4">Login</button>
+                      </div>
+              </div>
+              <div class="col-6 mt-4">
+                     @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                        </div>
+                </div>
+          </div>
+
+         
+        </form><!-- END -->
+    </div> <!-- .col-md-8 -->
+    </div>
+  </div>
+</div>
+</section> <!-- .section -->
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +73,108 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+
+<section class="home-slider owl-carousel">
+
+    <div class="slider-item" style="background-image: url({{ asset('assets/images/bg_2.jpg')}});"
+        data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row slider-text justify-content-center align-items-center">
+
+                <div class="col-md-7 col-sm-12 text-center ftco-animate">
+                    <h1 class="mb-3 mt-5 bread">Register</h1>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="{{ route('login') }}">Home</a></span>
+                        <span>Register</span></p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ftco-animate">
+                <form method="POST" action="{{ route('register') }}" class="billing-form ftco-bg-dark p-3 p-md-5">
+                    @csrf
+                    <h3 class="mb-4 billing-heading">Register</h3>
+                    <div class="row align-items-end">
+                        <div class="col-md-12">
+                            <label for="name" class="col-md-4 col-form-label">{{ __('Name') }}</label>
+
+                            <div class="form-group">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                    name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="email"
+                                        class="col-md-4 col-form-label">{{ __('Email Address') }}</label>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="password"
+                                        class="col-md-4 col-form-label">{{ __('Password') }}</label>
+
+
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="new-password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="password-confirm"
+                                        class="col-md-4 col-form-label">{{ __('Confirm Password') }}</label>
+
+                                    <input id="password-confirm" type="password" class="form-control"
+                                            name="password_confirmation" required autocomplete="new-password">
+                                    
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <div class="radio">
+                                        <button type="submit" class="btn btn-primary py-3 px-4">
+                                            {{ __('Register') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                </form><!-- END -->
+            </div> <!-- .col-md-8 -->
+        </div>
+    </div>
+    </div>
+</section> <!-- .section -->
 @endsection
